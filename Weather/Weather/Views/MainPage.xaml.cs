98c96b6c5143ab529ab7ec30d6fe7cb72f860a2d
Olga Boiarshynova <37959990+OlgaBoiarshynova@users.noreply.github.com>
2018-04-04 02:19:15 +0300

    Base UI controls are added

---
+++ Weather/Weather/Views/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Weather.Models;
+using Xamarin.Forms;
+
+namespace Weather
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var src = new List<CurrentConditions>();
+            src.Add(new CurrentConditions());
+
+            listView.ItemsSource = src;
+        }
+
+        private void OnDeleteClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
