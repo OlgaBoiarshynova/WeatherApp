ba6ada4733952c733014891674a273692e21373c
Olga Boiarshynova <37959990+OlgaBoiarshynova@users.noreply.github.com>
2018-04-04 00:09:43 +0300

    Data Models are added

---
+++ Weather/Weather/Models/Imperial.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Weather.Models
+{
+    public class Imperial
+    {
+        public int Value { get; set; }
+        public string Unit { get; set; }
+        public int UnitType { get; set; }
+    }
+}
