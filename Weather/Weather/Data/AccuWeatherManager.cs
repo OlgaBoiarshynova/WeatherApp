a1f0515088a7f82e13f109388b7af9259014be65
Olga Boiarshynova <37959990+OlgaBoiarshynova@users.noreply.github.com>
2018-04-04 01:15:40 +0300

    Data manager is added

---
+++ Weather/Weather/Data/AccuWeatherManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Weather.Models;
+
+namespace Weather.Data
+{
+    public class AccuWeatherManager
+    {
+        IAccuWeatherService restService;
+
+        public AccuWeatherManager(IAccuWeatherService restService)
+        {
+            this.restService = restService;
+        }
+
+        public Task<List<Location>> GetLocations(string searchText)
+        {
+            return restService.GetLocations(searchText);
+        }
+
+        public Task<List<CurrentConditions>> GetCurrentConditions(int locationKey)
+        {
+            return restService.GetCurrentConditions(locationKey);
+        }
+    }
+}
