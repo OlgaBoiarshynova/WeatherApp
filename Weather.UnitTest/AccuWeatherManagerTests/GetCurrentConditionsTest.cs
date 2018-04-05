3f5d75e7867406f3f38b5917db772e7b7e6feb0e
Olga Boiarshynova <37959990+OlgaBoiarshynova@users.noreply.github.com>
2018-04-05 23:15:16 +0300

    NUnit test project is added

---
+++ Weather.UnitTest/AccuWeatherManagerTests/GetCurrentConditionsTest.cs
@@ -0,0 +1,48 @@
+﻿using Moq;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Weather.Data;
+using Weather.Models;
+
+namespace Weather.AccuWeatherManagerTests.UnitTest
+{
+    [TestFixture]
+    class GetCurrentConditionsTest
+    {
+        private const string CONDITIONS_RESPONSE_JSON = "[{\"LocalObservationDateTime\": \"2018-04-03T15:15:00-05:00\",\"EpochTime\": 1522786500,\"WeatherText\": \"Light rain\",\"WeatherIcon\": 12,\"IsDayTime\": true,\"Temperature\": {\"Metric\": {\"Value\": 23.9,\"Unit\": \"C\",\"UnitType\": 17},\"Imperial\": {\"Value\": 75,\"Unit\": \"F\",\"UnitType\": 18}},\"MobileLink\": \"http://m.accuweather.com/en/us/odessa-tx/79761/current-weather/331122?lang=en-us\",\"Link\": \"http://www.accuweather.com/en/us/odessa-tx/79761/current-weather/331122?lang=en-us\"}]";
+        private Mock<IAccuWeatherService> mock;
+        private AccuWeatherManager manager;
+
+        [OneTimeSetUp]
+        public void FixtureSetup()
+        {
+            var conditions = JsonConvert.DeserializeObject<List<CurrentConditions>>(CONDITIONS_RESPONSE_JSON);
+            mock = new Mock<IAccuWeatherService>();
+            mock.Setup(d => d.GetCurrentConditions("331122")).ReturnsAsync(conditions);
+            manager = new AccuWeatherManager(mock.Object, new Dictionary<string, Object>());
+        }
+
+        [Category("AccuWeatherManager GetCurrentConditions")]
+        [Test]
+        public async Task GetCurrentConditionsResultCountTest()
+        {
+            var callResult = await manager.GetCurrentConditions("331122");
+            Assert.AreEqual(1, callResult.Count);
+        }
+
+        [Category("AccuWeatherManager GetCurrentConditions")]
+        [Test]
+        public async Task GetCurrentConditionsWeatherTextTest()
+        {
+            var callResult = await manager.GetCurrentConditions("331122");
+            Assert.AreEqual("Light rain", callResult[0].WeatherText);
+        }
+        //		Description	"Light rain (23,9℃ / 75℉) | Observed at 03.04.2018 23:15:00"	string
+
+    }
+}
---
+++ Weather.UnitTest/AccuWeatherManagerTests/GetLocationsTest.cs
@@ -0,0 +1,46 @@
+﻿using Moq;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Weather.Data;
+using Weather.Models;
+
+namespace Weather.AccuWeatherManagerTests.UnitTest
+{
+    [TestFixture]
+    class GetLocationsTest
+    {
+        private const string ODESSA_RESPONSE_JSON = "[{ \"Version\": 1,\"Key\": \"331122\",\"Type\": \"City\",\"Rank\": 45,\"LocalizedName\": \"Odessa\",\"EnglishName\": \"Odessa\",\"PrimaryPostalCode\": \"79761\",\"Region\": {\"ID\": \"NAM\",\"LocalizedName\": \"North America\",\"EnglishName\": \"North America\"},\"Country\": {\"ID\": \"US\",\"LocalizedName\": \"United States\",\"EnglishName\": \"United States\"},\"AdministrativeArea\": {\"ID\": \"TX\",\"LocalizedName\": \"Texas\",\"EnglishName\": \"Texas\",\"Level\": 1,\"LocalizedType\": \"State\",\"EnglishType\": \"State\",\"CountryID\": \"US\"},\"TimeZone\": {\"Code\": \"CDT\",\"Name\": \"America/Chicago\",\"GmtOffset\": -5,\"IsDaylightSaving\": true,\"NextOffsetChange\": \"2018-11-04T07:00:00Z\"},\"GeoPosition\": {\"Latitude\": 31.846,\"Longitude\": -102.368,\"Elevation\": {\"Metric\": {\"Value\": 887,\"Unit\": \"m\",\"UnitType\": 5},\"Imperial\": {\"Value\": 2909,\"Unit\": \"ft\",\"UnitType\": 0}}},\"IsAlias\": false,\"SupplementalAdminAreas\": [{\"Level\": 2,\"LocalizedName\": \"Ector\",\"EnglishName\": \"Ector\"}],\"DataSets\": [\"Alerts\",\"DailyAirQualityForecast\",\"DailyPollenForecast\",\"ForecastConfidence\",\"MinuteCast\"]},]";
+        private Mock<IAccuWeatherService> mock;
+        private AccuWeatherManager manager;
+
+        [OneTimeSetUp]
+        public void FixtureSetup()
+        {
+            var locations = JsonConvert.DeserializeObject<List<Location>>(ODESSA_RESPONSE_JSON);
+            mock = new Mock<IAccuWeatherService>();
+            mock.Setup(d => d.GetLocations("Odessa")).ReturnsAsync(locations);
+            manager = new AccuWeatherManager(mock.Object, new Dictionary<string, Object>());
+        }
+
+        [Category("AccuWeatherManager GetLocations")]
+        [Test]
+        public async Task GetLocationsResultCountTest()
+        {                   
+            var callResult =  await manager.GetLocations("Odessa");
+            Assert.AreEqual(1, callResult.Count);
+        }
+
+        [Category("AccuWeatherManager GetLocations")]
+        [Test]
+        public async Task GetLocationsKeyTest()
+        {
+            var callResult = await manager.GetLocations("Odessa");
+            Assert.AreEqual("331122", callResult[0].Key);
+        }
+    }
+}
