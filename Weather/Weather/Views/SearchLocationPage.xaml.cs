65e5f8094c991693588b318eac60419d95035cdd
Olga Boiarshynova <37959990+OlgaBoiarshynova@users.noreply.github.com>
2018-04-05 21:07:06 +0300

    ActivityIndicator is added

--- Weather/Weather/Views/SearchLocationPage.xaml.cs
+++ Weather/Weather/Views/SearchLocationPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Windows.Input;
 using Weather.Models;
-using Windows.UI.Core;
+//using Windows.UI.Core;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -18,24 +18,24 @@ namespace Weather.Views
         public SearchLocationPage()
         {
             InitializeComponent();
-            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+            //SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
             Items = new ObservableCollection<Location>();
 			locationsListView.ItemsSource = Items;
         }
 
         async void Handle_ItemTapped(object sender, ItemTappedEventArgs e)
         {
-            if (e.Item == null)
-                return;
-            App.WeatherManager.addLocation(e.Item as Location);
-
+            if (e.Item == null) return;
+            App.WeatherManager.AddLocation(e.Item as Location);
             //Deselect Item
             ((ListView)sender).SelectedItem = null;
         }
 
         private async void OnSearchRequested(object sender, EventArgs e)
         {
+            activityIndicator.IsVisible = true;
             locationsListView.ItemsSource = await App.WeatherManager.GetLocations(searchBar.Text);
+            activityIndicator.IsVisible = false;
         }
     }
 }
